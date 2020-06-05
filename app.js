1c172518c16c01aad3d33a473ea655ef9e020ca6
Sander Nobel <53917461+sano2019@users.noreply.github.com>
2020-06-05 10:09:32 +0200

    Initial commit through browser

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.get("/", function(req, res){
+	var today = new Date();
+	var currentDay = today.getDay();
+
+	if (currentDay == 6 || currentDay == 0) {
+		res.write("<h1>Yay, It is the weekend!</h1>")
+	} else {
+		res.write("<h1>Boo! I have to work :(</h1>")
+	}
+
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function(){
+	console.log("Server started on port 3000")
+});
